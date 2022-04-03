4cb720ea953821a7e47eb1a2846be53fe6d631e5
Tad <a@as-MacBook-Pro.local>
2022-04-03 15:50:02 -0400

    delineateBridgeSections

--- src/Experience/Timeline/Sections/Bridge/Line1.js
+++ src/Experience/Timeline/Sections/Bridge/Line1.js
@@ -143,15 +143,15 @@ export function timeline_bridge_line_1(timeline) {
         // endAt: 1250
         // startAt: 0,
         // endAt: 1.25,
-        startAt: 74.8,
-        endAt: 77.0,
+        startAt: 77.7,
+        endAt: 80.3,
       },
     ],
     started: false,
     // startAt: 0,
     // endAt: 1.25,
-    startAt: 74.8,
-    endAt: 77.9,
+    startAt: 77.7,
+    endAt: 80.3,
   };
 
   // --- --- --- Hurry up
@@ -194,14 +194,488 @@ export function timeline_bridge_line_1(timeline) {
         // endAt: 1250
         // startAt: 0,
         // endAt: 1.25,
-        startAt: 74.8,
-        endAt: 77.0,
+        startAt: 80.3,
+        endAt: 83.0,
       },
     ],
     started: false,
     // startAt: 0,
     // endAt: 1.25,
-    startAt: 74.8,
-    endAt: 77.9,
+    startAt: 80.3,
+    endAt: 83.0,
+  };
+
+  // --- --- --- are forgotten
+
+  timeline[uuidv4()] = {
+    instanceName: INSTANCE_NAMES.LINE_1,
+    effects: [
+      {
+        name: EFFECTS.FROM_TO,
+        properties: [
+          {
+            path: "mesh.position",
+            from: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+            to: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+          },
+          {
+            path: "mesh.scale",
+            from: {
+              x: 0.05,
+              y: 0.05,
+              z: 0.05,
+            },
+            to: {
+              x: 3.0,
+              y: 0.05,
+              z: 0.05,
+            },
+          },
+        ],
+        // startAt: 0,
+        // endAt: 1250
+        // startAt: 0,
+        // endAt: 1.25,
+        startAt: 83.0,
+        endAt: 85.6,
+      },
+    ],
+    started: false,
+    // startAt: 0,
+    // endAt: 1.25,
+    startAt: 83.0,
+    endAt: 85.6,
+  };
+
+  // --- --- --- Hallelujah
+  // --- --- --- Hallelujah
+
+  timeline[uuidv4()] = {
+    instanceName: INSTANCE_NAMES.LINE_1,
+    effects: [
+      {
+        name: EFFECTS.FROM_TO,
+        properties: [
+          {
+            path: "mesh.position",
+            from: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+            to: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+          },
+          {
+            path: "mesh.scale",
+            from: {
+              x: 0.05,
+              y: 0.05,
+              z: 0.05,
+            },
+            to: {
+              x: 3.0,
+              y: 0.05,
+              z: 0.05,
+            },
+          },
+        ],
+        // startAt: 0,
+        // endAt: 1250
+        // startAt: 0,
+        // endAt: 1.25,
+        startAt: 85.6,
+        endAt: 89.2,
+      },
+    ],
+    started: false,
+    // startAt: 0,
+    // endAt: 1.25,
+    startAt: 85.6,
+    endAt: 89.2,
+  };
+
+  // --- --- --- Hallelujah
+
+  timeline[uuidv4()] = {
+    instanceName: INSTANCE_NAMES.LINE_1,
+    effects: [
+      {
+        name: EFFECTS.FROM_TO,
+        properties: [
+          {
+            path: "mesh.position",
+            from: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+            to: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+          },
+          {
+            path: "mesh.scale",
+            from: {
+              x: 0.05,
+              y: 0.05,
+              z: 0.05,
+            },
+            to: {
+              x: 3.0,
+              y: 0.05,
+              z: 0.05,
+            },
+          },
+        ],
+        // startAt: 0,
+        // endAt: 1250
+        // startAt: 0,
+        // endAt: 1.25,
+        startAt: 89.2,
+        endAt: 93.3,
+      },
+    ],
+    started: false,
+    // startAt: 0,
+    // endAt: 1.25,
+    startAt: 89.2,
+    endAt: 93.3,
+  };
+
+  /* Repeat */
+
+  timeline[uuidv4()] = {
+    instanceName: INSTANCE_NAMES.LINE_1,
+    effects: [
+      {
+        name: EFFECTS.FROM_TO,
+        properties: [
+          {
+            path: "mesh.position",
+            from: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+            to: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+          },
+          {
+            path: "mesh.scale",
+            from: {
+              x: 0.05,
+              y: 0.05,
+              z: 0.05,
+            },
+            to: {
+              x: 3.0,
+              y: 0.05,
+              z: 0.05,
+            },
+          },
+        ],
+        // startAt: 0,
+        // endAt: 1250
+        // startAt: 0,
+        // endAt: 1.25,
+        startAt: 93.3,
+        endAt: 95.8,
+      },
+    ],
+    started: false,
+    // startAt: 0,
+    // endAt: 1.25,
+    startAt: 93.3,
+    endAt: 95.8,
+  };
+
+  // It's time to say
+  // the things you've
+  timeline[uuidv4()] = {
+    instanceName: INSTANCE_NAMES.LINE_1,
+    effects: [
+      {
+        name: EFFECTS.FROM_TO,
+        properties: [
+          {
+            path: "mesh.position",
+            from: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+            to: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+          },
+          {
+            path: "mesh.scale",
+            from: {
+              x: 0.05,
+              y: 0.05,
+              z: 0.05,
+            },
+            to: {
+              x: 3.0,
+              y: 0.05,
+              z: 0.05,
+            },
+          },
+        ],
+        // startAt: 0,
+        // endAt: 1250
+        // startAt: 0,
+        // endAt: 1.25,
+        startAt: 95.8,
+        endAt: 98.5,
+      },
+    ],
+    started: false,
+    // startAt: 0,
+    // endAt: 1.25,
+    startAt: 95.8,
+    endAt: 98.5,
+  };
+
+  // always wanted
+  timeline[uuidv4()] = {
+    instanceName: INSTANCE_NAMES.LINE_1,
+    effects: [
+      {
+        name: EFFECTS.FROM_TO,
+        properties: [
+          {
+            path: "mesh.position",
+            from: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+            to: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+          },
+          {
+            path: "mesh.scale",
+            from: {
+              x: 0.05,
+              y: 0.05,
+              z: 0.05,
+            },
+            to: {
+              x: 3.0,
+              y: 0.05,
+              z: 0.05,
+            },
+          },
+        ],
+        // startAt: 0,
+        // endAt: 1250
+        // startAt: 0,
+        // endAt: 1.25,
+        startAt: 98.5,
+        endAt: 101.3,
+      },
+    ],
+    started: false,
+    // startAt: 0,
+    // endAt: 1.25,
+    startAt: 98.5,
+    endAt: 101.3,
+  };
+
+  // hurry up
+  // before you
+  timeline[uuidv4()] = {
+    instanceName: INSTANCE_NAMES.LINE_1,
+    effects: [
+      {
+        name: EFFECTS.FROM_TO,
+        properties: [
+          {
+            path: "mesh.position",
+            from: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+            to: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+          },
+          {
+            path: "mesh.scale",
+            from: {
+              x: 0.05,
+              y: 0.05,
+              z: 0.05,
+            },
+            to: {
+              x: 3.0,
+              y: 0.05,
+              z: 0.05,
+            },
+          },
+        ],
+        startAt: 101.3,
+        endAt: 103.8,
+      },
+    ],
+    started: false,
+    startAt: 101.3,
+    endAt: 103.8,
+  };
+
+  // are forgotten
+  timeline[uuidv4()] = {
+    instanceName: INSTANCE_NAMES.LINE_1,
+    effects: [
+      {
+        name: EFFECTS.FROM_TO,
+        properties: [
+          {
+            path: "mesh.position",
+            from: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+            to: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+          },
+          {
+            path: "mesh.scale",
+            from: {
+              x: 0.05,
+              y: 0.05,
+              z: 0.05,
+            },
+            to: {
+              x: 3.0,
+              y: 0.05,
+              z: 0.05,
+            },
+          },
+        ],
+        startAt: 103.8,
+        endAt: 106.7,
+      },
+    ],
+    started: false,
+    startAt: 103.8,
+    endAt: 106.7,
+  };
+
+  // Hallelujah
+  // Hallelujah
+  timeline[uuidv4()] = {
+    instanceName: INSTANCE_NAMES.LINE_1,
+    effects: [
+      {
+        name: EFFECTS.FROM_TO,
+        properties: [
+          {
+            path: "mesh.position",
+            from: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+            to: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+          },
+          {
+            path: "mesh.scale",
+            from: {
+              x: 0.05,
+              y: 0.05,
+              z: 0.05,
+            },
+            to: {
+              x: 3.0,
+              y: 0.05,
+              z: 0.05,
+            },
+          },
+        ],
+        startAt: 106.7,
+        endAt: 110,
+      },
+    ],
+    started: false,
+    startAt: 106.7,
+    endAt: 110,
+  };
+
+  // Hallelujah
+  timeline[uuidv4()] = {
+    instanceName: INSTANCE_NAMES.LINE_1,
+    effects: [
+      {
+        name: EFFECTS.FROM_TO,
+        properties: [
+          {
+            path: "mesh.position",
+            from: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+            to: {
+              x: 0,
+              y: 0,
+              z: 1,
+            },
+          },
+          {
+            path: "mesh.scale",
+            from: {
+              x: 0.05,
+              y: 0.05,
+              z: 0.05,
+            },
+            to: {
+              x: 3.0,
+              y: 0.05,
+              z: 0.05,
+            },
+          },
+        ],
+        startAt: 110,
+        endAt: 114.2,
+      },
+    ],
+    started: false,
+    startAt: 110,
+    endAt: 114.2,
   };
 }
